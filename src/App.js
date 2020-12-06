5c806ee6860764007fd7c2868bab77c91eba6bde
ryan hatfield <hatfield086@gmail.com>
2020-12-06 01:06:11 -0600

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-
+import React, { useState } from "react";
+import Header from "./components/htmlHeader";
+import Main from "./components/htmlMain";
+import Users from "./user.json";
 function App() {
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState(false);
+  const [users, setUsers] = useState(Users);
 
-  function filterName()
-  {
+  function searchName(event) {
+    setSearch(event.target.value)
+  };
 
-  }
+  function filterName() {
+    if (!sort) {
+      setUsers(users.sort((a, b) => (a.name > b.name) ? 1 : -1));
+      setSort(true);
+    } else {
+      setUsers(users.sort((a, b) => (a.name > b.name) ? -1 : 1));
+      setSort(false);
+    }
+  };
 
   function filterDepartment() {
-
-  }
- 
+    if (!sort) {
+      setUsers(users.sort((a, b) => (a.department > b.department) ? 1 : -1));
+      setSort(true);
+    } else {
+      setUsers(users.sort((a, b) => (a.department > b.department) ? -1 : 1));
+      setSort(false);
+    }
+  };
+  return (
+    <div>
+      <Header
+        search={search}
+        onSearch={searchName}
+        filterName={filterName}
+        filterDepartment={filterDepartment}
+      />
+      <Main>
+        {Users.map(event => {
+          return (<Main {...event} />)
+        })}
+      </Main>
+    </div>
+  );
  
 }
 
